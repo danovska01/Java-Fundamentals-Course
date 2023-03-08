771eb46e60b815b940a88ded740d624bb95fc0ad
danovska01 <89780260+danovska01@users.noreply.github.com>
2023-03-08 21:43:12 +0200

    Add files via upload

---
+++ _23_Text_Processing_Ex/_06_ReplaceRepeatingChars2.java
@@ -0,0 +1,29 @@
+package _23_Text_Processing_Ex;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class _06_ReplaceRepeatingChars2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        char[] ch = scanner.nextLine().toCharArray();
+        List<Character> characterList = new ArrayList<>();
+        for (int i = 0; i < ch.length; i++) {
+            characterList.add(ch[i]);
+        }
+
+        for (int i = 1; i < characterList.size(); i++) {
+            char previous = characterList.get(i - 1);
+            char current = characterList.get(i);
+
+            if (previous == current){
+                characterList.remove(i);
+                i--;
+            }
+        }
+        for (Character character : characterList) {
+            System.out.print(character);
+        }
+    }
+}
